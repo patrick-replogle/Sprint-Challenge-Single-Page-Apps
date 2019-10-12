4eb813c95cc21fe304249e015fd8c13faac16498
patrick-replogle <patrickr1138@gmail.com>
2019-10-12 09:11:25 -0700

    Added all of my routes.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
-import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
 
+import Nav from "./components/Nav.js";
+import CharacterList from "./components/CharacterList.js";
+import EpisodeList from "./components/EpisodeList.js";
+import LocationsList from "./components/LocationsList.js";
+import SearchForm from "./components/SearchForm.js";
+import WelcomePage from "./components/WelcomePage.js";
+import Header from "./components/Header.js";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Nav />
+      <Route path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
+      <Route path="/episodes" component={EpisodeList} />
+      <Route path="/locations" component={LocationsList} />
+      <Route path="/search" component={SearchForm} />
     </main>
   );
 }
---
+++ src/components/EpisodeList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import axios from "axios";
+
+const EpisodeList = () => {
+  return <div></div>;
+};
+
+export default EpisodeList;
