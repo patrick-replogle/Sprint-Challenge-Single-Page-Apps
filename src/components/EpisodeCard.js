1540ac7bb16c9f1327490136b16c882cdd70d6fd
patrick-replogle <patrickr1138@gmail.com>
2019-10-12 09:56:10 -0700

    episodes cards now displaying.

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -1,7 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
-const EpisodeCard = ({}) => {
-  return <div></div>;
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  alight-items: center;
+  justify-content: center;
+  width: 50%;
+  background-color: rgba(114, 121, 34, 0.9);
+  color: #5eb7b3;
+  margin: 20px 20px;
+  border-radius: 4px;
+  box-shadow: 2px 2px 10px 10px rgba(0, 0, 0, 0.1);
+  font-weight: bold;
+`;
+
+const EpisodeCard = ({ episode }) => {
+  return (
+    <Card>
+      <h1>Name: {episode.name}</h1>
+      <p>Air Date: {episode.air_date}</p>
+      <p>Episode Number: {episode.episode}</p>
+      <p>{episode.characters.length} characters featured in episode</p>
+    </Card>
+  );
 };
 
 export default EpisodeCard;
--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import EpisodeCard from "./EpisodeCard.js";
 
 const EpisodeList = () => {
-  return <div></div>;
+  const [episodes, setEpisodes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/episode/")
+      .then(response => {
+        console.log(response.data.results);
+        setEpisodes(response.data.results);
+      })
+      .catch(error => {
+        console.log(error, "error");
+      });
+  }, []);
+
+  return (
+    <div className="episodes-container">
+      {episodes.map((episode, index) => {
+        return <EpisodeCard episode={episode} key={index} />;
+      })}
+    </div>
+  );
 };
 
 export default EpisodeList;
